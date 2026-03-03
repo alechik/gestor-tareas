6b3dbebef837de755bae70f35c06596039be68bf
Omar Vallejos Peña <omalevp@hotmail.com>
2026-03-03 15:55:29 -0400

    creacion clase tarea

---
+++ src/tarea.py
@@ -0,0 +1,29 @@
+"""
+Clase Tarea.
+"""
+
+class Tarea:
+    def __init__(self, id, descripcion, completada=False):
+        """
+        Constructor de la clase.
+
+        id: Identificador único de la tarea
+        descripcion: Descripción de la tarea
+        completada: Estado de la tarea (true completado)
+        """
+        self.id = id
+        self.descripcion = descripcion
+        self.completada = completada
+
+    def marcar_completada(self):
+        """
+        Marca la tarea como completada.
+        """
+        self.completada = True
+
+    def __str__(self):
+        """
+        Muestra la tarea en consola.
+        """
+        estado = "✔" if self.completada else "✘"
+        return f"[{estado}] ({self.id}) {self.descripcion}"
